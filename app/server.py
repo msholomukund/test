f87efae2f611decda74d7a72f1456e9a1f25c9db
msholomukund <45308771+msholomukund@users.noreply.github.com>
2019-06-28 04:46:08 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -66,7 +66,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction, c2, c3 = learn.predict(img)
+    prediction  = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
 
